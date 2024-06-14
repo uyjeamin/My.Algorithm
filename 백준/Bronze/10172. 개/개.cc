0c752cfa0b4c4e92b5544ce031b8408b02bf60f7
jea_min <162968107+uyjeamin@users.noreply.github.com>
2024-06-14 20:14:11 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "|\\_/|\n";
+  cout << "|q p|   /}\n";
+  cout << "( 0 )\"\"\"\\\n";
+  cout << "|\"^\"`    |\n";
+  cout << "||_/=\\\\__|\n";
+
+    
+}
+
+// |\_/|
+// |q p|   /}
+// ( 0 )"""\
+// |"^"`    |
+// ||_/=\\__|
