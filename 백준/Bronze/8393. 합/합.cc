3131b71d58111294c461df3322f45b7e80500dd0
jea_min <162968107+uyjeamin@users.noreply.github.com>
2024-08-20 23:32:08 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int a =1;
+    cin >> n;
+    for(int i=1 ; i<=n ; i++){
+        a+=i;
+    }
+
+cout << a-1;
+    return 0;
+}
