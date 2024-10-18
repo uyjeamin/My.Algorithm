9f0a57da14fff256cba475721415cfde0442022f
jea_min <162968107+uyjeamin@users.noreply.github.com>
2024-10-18 12:01:23 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
@@ -1,9 +1,4 @@
 function solution(li) {
-    var answer = li.sort((a, b) => a - b);
-    console.log(answer)
-    answer = answer.slice(5)
-    console.log(answer)
-    
-    
-    return answer;
+    var answer = li.sort((a, b) => a - b);    
+    return li.slice(5);
 }
