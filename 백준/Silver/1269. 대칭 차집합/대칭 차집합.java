0cd23270105fad4256544f1b5d672b47e742225d
RyuJeaMin <162968107+uyjeamin@users.noreply.github.com>
2024-12-26 23:24:43 +0900

    [Silver IV] Title: 대칭 차집합, Time: 784 ms, Memory: 105648 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		Set A = new HashSet();
+		
+		Set Aclone = new HashSet();
+		
+		Set B = new HashSet();
+		
+		Set Bclone = new HashSet();
+		
+		StringTokenizer st1 = new StringTokenizer(br.readLine());
+	
+		for(int i=0 ; i<n; i++){
+		    int input = Integer.parseInt(st1.nextToken());
+		    A.add(input);
+		    Aclone.add(input);
+		}
+
+		
+		
+		StringTokenizer st2 = new StringTokenizer(br.readLine());
+	
+		for(int i=0 ; i<m; i++){
+		    int input = Integer.parseInt(st2.nextToken());
+		    B.add(input);
+		    Bclone.add(input);
+		}
+		
+		Aclone.removeAll(B);
+		Bclone.removeAll(A);
+		
+		bw.write(String.valueOf(Aclone.size()+Bclone.size()));
+		
+    
+		bw.close();
+		br.close();
+	}
+}
