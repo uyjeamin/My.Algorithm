8388cc67f90d516ac2a6886babf9601cc8f10e4f
RyuJeaMin <162968107+uyjeamin@users.noreply.github.com>
2025-02-10 23:31:14 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 192 ms, Memory: 17932 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        
+		String st;
+        while(sc.hasNextLine()){
+            st = sc.nextLine();
+            System.out.println(st);
+        }
+        
+	}
+}
