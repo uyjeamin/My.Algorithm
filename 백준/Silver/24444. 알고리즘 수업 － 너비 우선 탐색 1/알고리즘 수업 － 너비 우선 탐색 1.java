a8266ebe1a5699064c2f0e2a55122fd0eeb3e308
Ryujeamin <162968107+uyjeamin@users.noreply.github.com>
2025-08-19 14:29:35 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1032 ms, Memory: 90440 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    static ArrayList<ArrayList<Integer>> gragh = new ArrayList();
+    static int[] check;
+    static int count = 1;
+    
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int r = Integer.parseInt(st.nextToken());
+		
+		check = new int[n+1];
+		
+		for(int i=0 ; i<=n ; i++) {
+		    gragh.add(new ArrayList<>());
+		}
+		
+		for (int i=0 ; i<m ; i++) {
+		    StringTokenizer st1 = new StringTokenizer(br.readLine());
+		    int ver1 = Integer.parseInt(st1.nextToken());
+		    int ver2 = Integer.parseInt(st1.nextToken());
+		    
+		    gragh.get(ver1).add(ver2);
+		    gragh.get(ver2).add(ver1);
+		}
+		
+        for (int i=0 ; i<n ; i++) {
+ 		    Collections.sort(gragh.get(i));
+ 		}
+		
+		/*
+		for (int i=0 ; i<n ; i++) {
+		    for (int j=0 ; j<gragh.get(i).size() ; j++) {
+		       System.out.print(gragh.get(i).get(j)+" ");
+		    }
+		    System.out.println();
+ 		} */
+ 		
+		BFS(r);
+		
+		for (int i=1 ; i<=n ; i++) {
+		    bw.write(check[i]+"\n");
+		}
+
+		bw.close();
+		br.close();
+	}
+	
+	public static void BFS(int r) {
+	     Queue<Integer> queue = new LinkedList<>();
+	     check[r] = count++;
+	     queue.add(r);
+	     
+	     while (!queue.isEmpty()) {
+	         int vertex = queue.poll();
+	         
+	         
+	         //System.out.println("그래프 사이즈"+gragh.get(vertex).size());
+	         //System.out.println("vertex "+vertex);
+	         for (int i=0 ; i<gragh.get(vertex).size() ; i++) {
+	             int nextVertex = gragh.get(vertex).get(i);
+	             
+	             //System.out.println("nextVertex "+nextVertex);
+	             
+	             if (check[nextVertex] == 0) {
+	                check[nextVertex] = count++; 
+	                queue.add(nextVertex); 
+	             } 
+	         }
+	     }
+	}
+}
