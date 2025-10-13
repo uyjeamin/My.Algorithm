8cde5c70b1b1b43aab384b1ba141dc21bc036341
Ryujeamin <162968107+uyjeamin@users.noreply.github.com>
2025-10-13 10:53:36 +0900

    [Silver III] Title: N과 M (3), Time: 516 ms, Memory: 158052 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    
+    static int[] arr;
+    static boolean[] IsVisited;
+    static int n,m;
+    static BufferedReader br;
+    static BufferedWriter bw;
+    
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+	    arr = new int[m];
+	    IsVisited = new boolean[n+1];
+	    
+	    dfsBacktracking(0);
+		
+	
+		bw.close();
+		br.close();
+	}
+	
+	static public void dfsBacktracking(int depth) throws IOException {
+	    if (depth == m) {
+	        for(int i=0 ; i<m ; i++) {
+	            bw.write(arr[i]+" ");
+	        }
+	        bw.write("\n");
+	        return ;
+	    }
+	    
+	    for (int i=1; i<=n; i++) {
+	            arr[depth] = i;
+	            dfsBacktracking(depth+1);
+	    }
+	}
+}
