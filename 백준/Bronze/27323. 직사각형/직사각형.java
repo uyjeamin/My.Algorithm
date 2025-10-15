22341c6212644c9b17a96bc13d5b9efd2d6f1148
Ryujeamin <162968107+uyjeamin@users.noreply.github.com>
2025-10-15 23:22:45 +0900

    [Bronze V] Title: 직사각형, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		
+		bw.write(a*b+"");
+		
+		
+		
+		bw.close();
+		br.close();
+	}
+}
